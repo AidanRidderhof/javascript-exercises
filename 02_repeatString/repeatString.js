79731b5749dd554e3a41f8ec18b762e183bf41b1
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2024-07-12 17:35:34 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    if (count < 0) {
+        return "ERROR";
+    }
+    let bigWord =''
+    while (count>0) {
+        bigWord+=word;
+        count--;
+    }
+    return bigWord;
 };
 
 // Do not edit below this line
