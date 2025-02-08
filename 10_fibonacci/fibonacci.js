f7156619d7a558a579e06ce0f665397b32af8056
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2025-02-08 18:30:25 -0500

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+
+    if (num<0) {
+        return "OOPS";
+    }
+    if (num==0) {
+        return 0;
+    }
+
+    var arr = [];
+    for (let i = 0; i<=num; i++){
+        if (i==0 || i==1){
+            arr[i]=1;
+        }
+        else {
+            arr[i]= arr[i-1]+arr[i-2];
+        }
+    }
+    return arr[num-1];
 };
 
 // Do not edit below this line
