d77ba284c6c204d333bed9509c97c80eb0a4dffd
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2025-02-09 00:16:08 -0500

    finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    const order = arr.sort(function(a,b) {
+        if (a.yearOfDeath==null){
+            a.yearOfDeath=2024;
+        }
+        if (b.yearOfDeath==null){
+            b.yearOfDeath=2024;
+        }
+
+        if ((a.yearOfDeath-a.yearOfBirth) < (b.yearOfDeath-b.yearOfBirth)) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    })
+
+    return order[0];
 };
 
 // Do not edit below this line
