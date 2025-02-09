977863bb35ba130069dab0bf7d33010a7abde002
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2025-02-09 01:05:40 -0500

    finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let cleanstr = string
+    .replace(/[^a-zA-Z0-9]/g, '')
+    .toLowerCase()
+    .replace(' ', '');
+
+    let reversestr = cleanstr
+        .split('')
+        .reverse()
+        .join('');
+    
+    
+    if (cleanstr == reversestr) {
+        return true
+    }
+
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
